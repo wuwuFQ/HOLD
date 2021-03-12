1926f004de86438362ad9debfe47ce433eb19825
wuwuFQ <13301325631@163.com>
2021-03-12 10:10:32 +0800

    first commit

--- app.js
+++ app.js
@@ -1,19 +1,64 @@
 // app.js
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
+    //同步获取用户信息
+    var info = wx.getStorageSync('userInfo');
+    if (!this.isNull(info)) {
+      this.globalData.userInfo = info;
+      this.globalData.openid = info.openid;
+    } else {
+      // 登录   在index做了
+      // this.wxLogin();
+    }
+  },
+  globalData: {
+    openid: null,
+    userInfo: null,
+    // httpURL: 'https://localhost/',   //测试
+    httpURL: 'https://wuwufq.asia/',   //正式
+
+  },
+
+  wxLogin() {
+    var that = this;
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res);
+        // 发送 res.code 到后台换取 openId（otvsv5ai27v_Wu3r55-vA1KJE0lU）, sessionKey, unionId
+        that.getWeChatOpenID(res.code);
+      }
+    })
+
+  },
+ // 获取openID
+  getWeChatOpenID: function (code) {
+     
+    var that = this;
+    wx.request({
+      url: that.globalData.httpURL + 'wechat/getHoldOpenID',
+      data: {
+        code: code
+      },
+      success: res => {
+        let response = res.data;
+        if (response.code == 10000) {
+          let openid = response.data.openid;
+          console.log("openid：" + openid);
+          that.globalData.openid = openid;
+        }
       }
     })
   },
-  globalData: {
-    userInfo: null
+
+ 
+  // obj是空值?
+  isNull: function (obj) {
+    if (obj == null || obj == "") {
+      return true;
+    } else {
+      return false;
+    }
   }
+
 })
--- pages/service/service.js
+++ pages/service/service.js
@@ -1,10 +1,15 @@
 // pages/service/service.js
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    dataArr: [],
+    // 是否已登录并完善信息
+    isLogin: false,
 
   },
 
@@ -12,7 +17,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getPeopleList();
   },
 
   /**
@@ -26,6 +31,16 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    if (app.isNull(app.globalData.userInfo)) {
+      this.setData({
+        isLogin: false,
+      })
+      this.wxLogin();
+    } else {
+      this.setData({
+        isLogin: true,
+      })
+    }
 
   },
 
@@ -64,9 +79,92 @@ Page({
 
   },
 
+  getPeopleList() {
+    var that = this;
+    wx.request({
+      url: app.globalData.httpURL + 'hold/service/peopleList',
+      method: 'GET',
+      success: res => {
+        let response = res.data;
+        if (response.code == 10000) {
+          that.setData({
+            dataArr: response.data,
+          })
+        } else {
+
+        }
+      }
+    })
+  },
+
   itemClickHandle(e) {
     wx.makePhoneCall({
       phoneNumber: '19902019616',
     })
-  }
+  },
+
+
+  wxLogin() {
+    var that = this;
+    wx.login({
+      success: res => {
+        console.log(res);
+        // 发送 res.code 到后台换取 openId（otvsv5ai27v_Wu3r55-vA1KJE0lU）, sessionKey, unionId
+        that.getWeChatOpenID(res.code);
+      }
+    })
+
+  },
+  // 获取openID
+  getWeChatOpenID: function (code) {
+    var that = this;
+    wx.request({
+      url: app.globalData.httpURL + 'wechat/getHoldOpenID',
+      data: {
+        code: code
+      },
+      success: res => {
+        let response = res.data;
+        if (response.code == 10000) {
+          let openid = response.data.openid;
+          console.log("openid：" + openid);
+          app.globalData.openid = openid;
+          that.getUserInfo(openid);
+        }
+      }
+    })
+  },
+  //根据openID获取用户信息 可能为null
+  getUserInfo: function (openid) {
+    var that = this;
+    app.globalData.openid = openid;
+    wx.request({
+      url: app.globalData.httpURL + 'hold/user/getUserInfo',
+      data: {
+        openid: openid
+      },
+      success: res => {
+        console.log("userInfo：" + res.data.data);
+        app.globalData.userInfo = res.data.data;
+        if (!app.isNull(app.globalData.userInfo)) {
+          that.setData({
+            isLogin: true,
+          })
+          wx.setStorage({
+            data: app.globalData.userInfo,
+            key: 'userInfo',
+          })
+        }
+      }
+    })
+  },
+
+  // 个人信息
+  userInfoClick(e) {
+    wx.navigateTo({
+      url: '../userInfo/userInfo',
+    })
+  },
+
+
 })
